5bb8bba43e5221386b25e3d5b787eb08a382dd4c
Igor Guerrero <igorgue@protonmail.com>
2024-01-09 22:28:08 -0600

    solve these stuff later, don't  waste too much of your   time

--- src/main.zig
+++ src/main.zig
@@ -11,10 +11,28 @@ fn activateCb(app: *c.GtkApplication, data: c.gpointer) callconv(.C) void {
 
     const window = c.gtk_application_window_new(app);
     const web_view = c.webkit_web_view_new();
+    const overlay = c.gtk_overlay_new();
+    const button = c.gtk_button_new();
 
     c.gtk_window_set_default_size(@as(*c.GtkWindow, @ptrCast(window)), 200, 200);
-    c.gtk_window_set_child(@as(*c.GtkWindow, @ptrCast(window)), web_view);
+    c.gtk_overlay_set_child(@as(*c.GtkOverlay, @ptrCast(overlay)), web_view);
+    c.gtk_window_set_child(@as(*c.GtkWindow, @ptrCast(window)), overlay);
+    c.gtk_overlay_add_overlay(@as(*c.GtkOverlay, @ptrCast(overlay)), button);
     c.gtk_window_present(@as(*c.GtkWindow, @ptrCast(window)));
+
+    // on click for trhe label reveal the web view
+    _ = c.g_signal_connect_data(
+        label,
+        "button-press-event",
+        @as(c.GCallback, @ptrCast(&|_, _| {
+            c.gtk_overlay_set_overlay_pass_through(@as(*c.GtkOverlay, @ptrCast(overlay)), web_view, true);
+            c.gtk_widget_hide(@as(*c.GtkWidget, @ptrCast(label)));
+            return false;
+        })),
+        null,
+        null,
+        0,
+    );
 
     c.webkit_web_view_load_uri(@as(*c.WebKitWebView, @ptrCast(web_view)), home);
 }
