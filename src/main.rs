0cb2de5f56bbbbcc5bc3b789512b18e29ae2c04e
Igor Guerrero <igorgue@protonmail.com>
2024-01-25 18:27:27 -0600

    add stuff

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use adw::prelude::*;
-
 use adw::gdk::{Key, ModifierType};
 use adw::gio::Cancellable;
 use adw::glib::Propagation;
 use adw::gtk::EventControllerKey;
+use adw::prelude::*;
 use adw::{Application, ApplicationWindow};
 
 use webkit::{prelude::*, LoadEvent, WebView};
