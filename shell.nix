c885d620a71a457e76d3a11a6745a6d97d52b7a7
Igor Guerrero <igorgue@protonmail.com>
2024-07-14 21:52:28 -0600

    Update shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> { };
 stdenv.mkDerivation {
   name = "frameless";
-  buildInputs = [ gtk4 libsoup_3 webkitgtk_6_0 libadwaita ];
+  buildInputs = [ pkg-config gtk4 libsoup_3 webkitgtk_6_0 libadwaita ];
 }
